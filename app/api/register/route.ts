c888c75ed2f7767adb0d3b8f46f54efc4bed49e5
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-03-19 14:13:32 +0000

    Added Registration

---
+++ app/api/register/route.ts
@@ -0,0 +1,16 @@
+import bcrypt, { hash } from "bcrypt"
+import prisma from "@/libs/prismadb"
+import { NextResponse } from "next/server"
+
+export async function POST(request: Request){
+  const body = await request.json()
+  const {name, password, email} = body
+  const hashedPassword = await hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      name, email, hashedPassword
+    }
+  })
+  return NextResponse.json(user)
+}
