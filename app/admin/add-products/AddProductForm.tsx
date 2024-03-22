b2bd05f3a60d7129e843073b2c5f3c97bd72fe39
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-03-22 14:49:50 +0000

    Add Product Logic

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,5 +1,195 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import CategoryInput from "@/app/components/inputs/CategoryInput";
+import CustomCheckBox from "@/app/components/inputs/CustomCheckBox";
+import Input from "@/app/components/inputs/Input";
+import SelectColor from "@/app/components/inputs/SelectColor";
+import TextArea from "@/app/components/inputs/TextArea";
+import Heading from "@/app/components/products/Heading";
+import { categories } from "@/utils/Categories";
+import { colors } from "@/utils/Colors";
+import { useCallback, useEffect, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { setConstantValue } from "typescript";
+
+export type ImageType = {
+  color: string;
+  colorCode: string;
+  image: File | null;
+};
+
+export type UploadedImageType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
+
 const AddProductForm = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState<ImageType[] | null>();
+  const [isProductCreated, setIsProductCreated] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      images: [],
+      price: "",
+      inStock: false,
+    },
+  });
+
+  useEffect(() => {
+    setCustomValue("images", images);
+  }, [images]);
+
+  useEffect(() => {
+    if (isProductCreated) {
+      reset();
+      setImages(null);
+      setIsProductCreated(false);
+    }
+  }, [isProductCreated]);
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    // upload images to fb
+    // save product to mongodb
+  };
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  const addImageToState = useCallback((value: ImageType) => {
+    setImages((prev) => {
+      if (!prev) {
+        return [value];
+      }
+      return [...prev, value];
+    });
+  }, []);
+
+  const removeImageFromState = useCallback((value: ImageType) => {
+    setImages((prev) => {
+      if (prev) {
+        const filteredImages = prev.filter(
+          (item) => item.color !== value.color
+        );
+        return filteredImages;
+      }
+      return prev;
+    });
+  }, []);
+
+  return (
+    <>
+      <Heading title="Add a Product" center />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="number"
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This Product is in Stock"
+      />
+      <div className="w-full font-medium  ">
+        <div className="mb-2 font-bold">Select a Category</div>
+        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto gap-3 ">
+          {categories.map((item) => {
+            if (item.label === "ALL") {
+              return null;
+            }
+
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col flex-wrap gap-4">
+        <div>
+          <div className="font-bold">
+            Select the available colors and upload their images.
+          </div>
+          <div className="text-sm">
+            You must upload an image for each of the selected color selected
+            otherwise your color selection will be ignored
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-3">
+          {colors.map((item, index) => {
+            return (
+              <SelectColor
+                addImageToState={addImageToState}
+                item={item}
+                key={index}
+                removeImageFromState={removeImageFromState}
+                isProductCreated={isProductCreated}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Button
+        label={isLoading ? "Loading..." : "Add Product"}
+        onClick={handleSubmit(onSubmit)}
+      />
+    </>
+  );
 };
 
 export default AddProductForm;
---
+++ utils/Categories.tsx
@@ -0,0 +1,29 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdStorefront, MdWatch } from "react-icons/md";
+
+export const categories = [
+  {
+    label: "ALL",
+    icon: MdStorefront,
+  },
+  {
+    label: "PHONE",
+    icon: AiFillPhone,
+  },
+  {
+    label: "Laptop",
+    icon: AiOutlineLaptop,
+  },
+  {
+    label: "Desktop",
+    icon: AiOutlineDesktop,
+  },
+  {
+    label: "Watch",
+    icon: MdWatch,
+  },
+  {
+    label: "Accessories",
+    icon: MdOutlineKeyboard,
+  },
+];
