561c9a6f253c7917475b1c8c727e9e2b7c9a727c
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-03-24 13:36:37 +0000

    Added Product Review

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -4,6 +4,8 @@ import ListRating from "./ListRating";
 import { products } from "@/utils/products";
 import getProductById from "@/actions/getProductById";
 import NullData from "@/app/components/NullData";
+import AddRating from "./AddRating";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 interface IPrams {
   productId?: string;
@@ -11,6 +13,7 @@ interface IPrams {
 
 const Product = async ({ params }: { params: IPrams }) => {
   const product = await getProductById(params);
+  const user = await getCurrentUser();
 
   if (!product)
     return <NullData title="Oops, Product with the given id does not exist" />;
@@ -20,7 +23,7 @@ const Product = async ({ params }: { params: IPrams }) => {
       <Container>
         <ProductDetails product={product} />
         <div className="flex flex-col mt-20 gap-4">
-          <div>Add Rating</div>
+          <AddRating user={user} product={product} />
           <ListRating product={product} />
         </div>
       </Container>
